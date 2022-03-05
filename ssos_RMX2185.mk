655a6b5794f7e8a1cb8cc52c101cc48dd895221a
Jomar <94120477+Neutralxs@users.noreply.github.com>
2022-03-05 22:14:51 +0800

    Update ssos_RMX2185.mk

--- ssos_RMX2185.mk
+++ ssos_RMX2185.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2185/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
+$(call inherit-product, vendor/ssos/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := ssos_RMX2185
